182e9559ba21c1ec25890f6ce629787e6e21e3bf
Kevin Lin <33496321+Stuy-Kevin-Lin@users.noreply.github.com>
2019-01-14 14:09:34 -0500

    Bugfix

--- util/dbtools.py
+++ util/dbtools.py
@@ -183,7 +183,7 @@ def changeGrades(classID, gradeList, assignment, maxGrade, weight):
     for i in gradeList:
         for j in c.execute("SELECT userID FROM grades WHERE classID = ? AND userID = ? AND assignment = ?", (classID, i[0], assignment,)):
             #Grade already exists, so modify
-            c.execute("UPDATE grades SET grade = ? WHERE classID = ? AND userID = ? AND assignment = ?", (grade, classID, i[0], assignment,))
+            c.execute("UPDATE grades SET grade = ?, maxGrade = ? WHERE classID = ? AND userID = ? AND assignment = ?", (grade, maxGrade, classID, i[0], assignment,))
             break
         else:
             #Otherwise create rows for grades

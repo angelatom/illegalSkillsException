6c0fa8437ae49eb97816f1380b14778e32952bdc
Angela <atom@stuy.edu>
2019-01-15 12:39:14 -0500

    finished quotes

--- util/quotes.py
+++ util/quotes.py
@@ -2,11 +2,11 @@ import json
 import requests
 from random import randint
 
-key = open("../keys/quotes_key.txt", "r").read()
+#key = open("../keys/quotes_key.txt", "r").read()
 
 def get_random_quote():
-    URL_STUB = "https://favqs.com/api/quotes/:"
-    rand_num = str(randint(1,100))
+    URL_STUB = "https://favqs.com/api/quotes/"
+    rand_num = str(randint(4,20))
     URL = URL_STUB + rand_num
     print(URL)
     r = requests.get(URL, headers={"Authorization": "Token token=941d1bc3cd831d275881fc9c87f04382","Content-Type": "application/json"})
@@ -17,6 +17,6 @@ def get_random_quote():
     #return data
     data = r.json()
    
-    return data
+    return data["body"]
 
 print(get_random_quote())
